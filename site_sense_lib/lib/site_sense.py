ac303507efcb6878ff1814543ad75c067065ce6d
Alexander R. Powell <161773628+arpowe11@users.noreply.github.com>
2024-09-25 18:06:42 -0400

    Update site_sense.py

--- site_sense_lib/lib/site_sense.py
+++ site_sense_lib/lib/site_sense.py
@@ -1,40 +1,46 @@
 import os
+
 from dotenv import load_dotenv, find_dotenv
-from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain.prompts import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+
+"""
+When using this AI model, if you want to get response, do:
+* response['response']  # get just the AI response
+"""
 
 
 class SiteSense:
     load_dotenv(find_dotenv(), override=True)
-    
+
     def __init__(self, *, model: str = "", temp: int = 0) -> None:
         self.llm = ChatOpenAI(model=model, temperature=temp)  # Initialize the LLM
-        self.message_history = ChatMessageHistory()  # Initialize message history
-    
-    def create_response(self, question: str) -> str:
+        self.memory = ConversationBufferMemory(memory_key="history", return_messages=True)
+
+    def generate_prompt(self) -> PromptTemplate:
         with open(os.environ["TEMPLATE_PATH"], 'r') as t:
             template = t.read()
 
-        history = "\n".join([msg.content for msg in self.message_history.messages])
-        prompt_template = PromptTemplate.from_template(template=template)
-        prompt = prompt_template.format(history=history, q=question)
+        # Define a prompt template
+        prompt_template = PromptTemplate(
+            input_variables=["history", "input"],
+            template=template
+        )
 
-        self.message_history.add_user_message(question)
-        return prompt
-    
-    def stream_response(self, prompt: str = "") -> str:
-        # Stream the output and accumulate it into a single message
-        ai_message = ""
-        for chunk in self.llm.stream(prompt):
-            print(chunk.content, end="")
-            ai_message += chunk.content  # Accumulate all chunks for the ai history
-        
-        self.message_history.add_ai_message(ai_message)
-        print("\n")
+        return prompt_template
 
-        return ai_message
-    
-    def clear_history(self):
-        self.message_history = ChatMessageHistory()
+    def chat_bot(self, user_input="") -> ConversationChain:
+        chatbot = ConversationChain(
+            llm=self.llm,
+            memory=self.memory,
+            prompt=self.generate_prompt(),
+        )
 
+        response = chatbot({"input": user_input})
+        return response
+
+    def clear_history(self) -> None:
+        self.memory = ConversationBufferMemory(memory_key="history", return_messages=True)
