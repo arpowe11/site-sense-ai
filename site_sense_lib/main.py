cf1156dddb0b7bf5cab0eb0952868916cbfe5066
Alexander R. Powell <161773628+arpowe11@users.noreply.github.com>
2024-09-25 18:07:16 -0400

    Update main.py

--- site_sense_lib/main.py
+++ site_sense_lib/main.py
@@ -1,17 +1,17 @@
-from lib.site_sense import SiteSense
+from site_sense_lib.lib.site_sense import SiteSense
 
 
 def main():
-    llm: SiteSense = SiteSense(model="gpt-3.5-turbo", temp=0.4)  # NOQA
+    emily: SiteSense = SiteSense(model="gpt-3.5-turbo", temp=0.4)  # NOQA
 
     while True:
         question = input("Site Sense: ")
         if question.lower() == 'q':
-            llm.clear_history()
+            emily.clear_history()
             break
 
-        prompt = llm.create_response(question=question)
-        llm.stream_response(prompt=prompt)
+        response = emily.chat_bot(question)
+        print(f"Emily: {response['response']}")
 
 if __name__ == "__main__":
     main()
